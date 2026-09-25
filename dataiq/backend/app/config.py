e629d0ca2e0639b44b7d7136b679d6b8d6fc1f90
justlugoo <maurogranados31@gmail.com>
2026-09-25 15:14:26 -0500

    feat(dataiq): agregar estructura hexagonal y configuración

---
+++ dataiq/backend/app/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Config(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    url_base_datos: str
+    sql_echo: bool = False
+
+
+config = Config()
